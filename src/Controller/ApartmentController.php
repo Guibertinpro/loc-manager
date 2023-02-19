65881a0e4edfe9543530729625e05968eccc743f
Guillaume BERTIN <80512116+Guibertinpro@users.noreply.github.com>
2023-02-19 22:18:30 +0100

    CRUD apartment

--- src/Controller/ApartmentController.php
+++ src/Controller/ApartmentController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Apartment;
 use App\Form\ApartmentType;
+use App\Repository\ApartmentRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -12,9 +13,13 @@ use Symfony\Component\Routing\Annotation\Route;
 class ApartmentController extends AbstractController
 {
   #[Route('/apartments/list', name: 'app_apartments_list')]
-  public function list()
+  public function list(ApartmentRepository $apartmentRepository)
   {
-    return $this->render('apartments/list.html.twig');
+    $apartments = $apartmentRepository->findAll();
+
+    return $this->render('apartments/list.html.twig', [
+      'apartments' => $apartments,
+    ]);
   }
 
   #[Route('/apartment/new', name: 'app_apartment_new')]
@@ -34,7 +39,42 @@ class ApartmentController extends AbstractController
         $entityManagerInterface->persist($apartment);
         $entityManagerInterface->flush();
 
-        return $this->redirectToRoute('app_home');
+        $this->addFlash('success', 'Apartement créé avec succès!');
+
+        return $this->redirectToRoute('app_apartments_list');
+    }
+
+    return $this->render('apartments/new.html.twig', [
+      'form' => $form
+    ]);
+  }
+
+  #[Route('/apartment/delete/{id}', name: 'app_apartment_delete', requirements: ['id' => '\d+'])]
+  public function delete(int $id, ApartmentRepository $apartmentRepository, EntityManagerInterface $entityManagerInterface)
+  {
+    $apartment = $apartmentRepository->find($id);
+
+    $apartmentRepository->remove($apartment);
+    $entityManagerInterface->flush();
+
+    return $this->redirectToRoute('app_apartments_list');
+  }
+
+  #[Route('/apartment/update/{id}', name: 'app_apartment_update')]
+  public function update(int $id, Request $request, EntityManagerInterface $entityManagerInterface, Apartment $apartment)
+  {
+    $form = $this->createForm(ApartmentType::class, $apartment);
+
+    $form->handleRequest($request);
+    if ($form->isSubmitted() && $form->isValid()) {
+        $apartment = $form->getData();
+
+        $entityManagerInterface->persist($apartment);
+        $entityManagerInterface->flush();
+
+        $this->addFlash('success', 'Apartement mis à jour avec succès!');
+
+        return $this->redirectToRoute('app_apartments_list');
     }
 
     return $this->render('apartments/new.html.twig', [
