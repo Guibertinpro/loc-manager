81ef4e9b2ffa67f8f53549c3f2a50532141bd508
Guillaume BERTIN <80512116+Guibertinpro@users.noreply.github.com>
2023-02-20 09:43:54 +0100

    Added translation for apartment form + Modified templates

--- src/Form/ApartmentType.php
+++ src/Form/ApartmentType.php
@@ -5,7 +5,9 @@ namespace App\Form;
 use App\Entity\Apartment;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ColorType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,20 +16,20 @@ class ApartmentType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name' )
-            ->add('address')
-            ->add('complement_address')
-            ->add('postcode')
-            ->add('city')
-            ->add('color', ColorType::class)
-            ->add('save', SubmitType::class)
+            ->add('name', TextType::class, ['label' => 'Nom de l\'appartement'])
+            ->add('address', TextType::class, ['label' => 'Adresse de l\'appartement'])
+            ->add('complement_address', TextType::class, ['label' => 'Complement de l\'appartement'])
+            ->add('postcode', NumberType::class, ['label' => 'Code postal'])
+            ->add('city', TextType::class, ['label' => 'Ville'])
+            ->add('color', ColorType::class, ['label' => 'Couleur'])
+            ->add('save', SubmitType::class, ['label' => 'Enregistrer'])
         ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-            'data_class' => Apartment::class,
+            'data_class' => Apartment::class
         ]);
     }
 }
